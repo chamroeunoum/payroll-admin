ac08cb159218a248a1559d84d3a944f174d3b5a5
Chamroeun OUM <chamroeunoum@gmail.com>
2026-09-16 11:01:27 +0700

    Update the commented local API port to 8888

--- src/store/index.js
+++ src/store/index.js
@@ -26,7 +26,7 @@ const debug = process.env.NODE_ENV !== 'production'
 
 export default createStore({
   state: {
-    // apiServer: 'http://127.0.0.1:8000/api/admin' ,
+    // apiServer: 'http://127.0.0.1:8888/api/admin' ,
     // apiServer: 'https://apis.ocm.gov.kh/api/admin' ,
     organization: {
       name: 'O AND M (CAMBODIA) CO., LTD'
